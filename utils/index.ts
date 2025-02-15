7f60bd25280a6184acfc1f8c2aa72132678af34f
felipeclarindo <fglpc2@gmail.com>
2025-02-15 03:16:06 -0300

    refactor(utils): Fix bugs of the functions and refactor the build.

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,23 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
-  const headers = {
-    "X-RapidAPI-Key": "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  const apiKey: string | undefined = process.env.RAPID_API_KEY;
+
+  if (!apiKey) {
+    throw new Error(
+      "API key is missing, please provide it in the environment variable RAPID_API_KEY."
+    );
+  }
+  const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": apiKey,
+      "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
+    },
   };
 
-  const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
-    { headers: headers }
-  );
+  const response = await fetch(url, options);
   return await response.json();
 }
 
@@ -24,4 +34,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimages");
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+  return `${url}`;
 };
