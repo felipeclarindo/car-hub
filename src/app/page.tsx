78fbeed732f02ccb6cf711c99479c98f7b5ffd66
felipeclarindo <fglpc2@gmail.com>
2025-02-15 03:14:44 -0300

    feat(app): Adding the animations of the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { fetchCars } from "../../utils";
 
 export default async function Home() {
   const allCars = await fetchCars();
-
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
-
   return (
     <main className="overflow-hidden">
       <Hero />
